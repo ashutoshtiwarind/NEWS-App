9583c56046229b031fff5f15f533250cd637e882
Ashutosh <ashutoshtiwarind@gmail.com>
2021-12-13 17:05:25 +0530

    project

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+function navbar() {
+  return `<div class="navbar">
+    <div>
+        <input id="news-name" type="text" />
+        <button id="search-news">search</button>
+    </div>
+  
+  <div class="right-header">
+    <div><a href="news.html">NEWS</a></div>
+    <div><a href="home.html">Home</a></div>
+    <div><a href="search.html">search</a></div>
+  </div>
+</div>`
+}
+
+export default navbar
